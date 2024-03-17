external-mirrors/pmbootstrap

commit 6ba138b6b2e3c57da12dc96922a1b01b775887c9

logging: print text between *** in green (MR 2263)
Highlight messages like the following from pmbootstrap install:
  [19:45:59] *** (1/4) PREPARE NATIVE CHROOT ***

[ci:skip-build]: already built successfully in CI

pmb/helpers/logging.py
@@ -47,6 +47,16 @@ class log_handler(logging.StreamHandler):
                         f"{styles['GREEN']}DONE!{styles['END']}",
                         1,
                     )
+                    .replace(
+                        "*** ",
+                        f"{styles['GREEN']}*** ",
+                        1,
+                    )
+                    .replace(
+                        " ***",
+                        f" ***{styles['END']}",
+                        1,
+                    )
                 )
 
                 stream.write(msg_col)

commit 96da7f161a9df425ca984eed5fffef3e43107a78

logging: reset style at the end of log line (MR 2272)
Fix a regression from my recent patch that made text like the following
green:
	*** text ****
We also have log messages that only start with ***, but do not end with
***, coming from "pmbootstrap aportgen". They indicate that a new aport
has been generated, but are not a headline for the coming commands.

Fix that the aportgen messages don't reset the colors at the end of the
line, and so the next line would start with the color.

Fixes: 6ba138b6 ("logging: print text between *** in green (MR 2263)")

pmb/helpers/logging.py
@@ -52,12 +52,9 @@ class log_handler(logging.StreamHandler):
                         f"{styles['GREEN']}*** ",
                         1,
                     )
-                    .replace(
-                        " ***",
-                        f" ***{styles['END']}",
-                        1,
-                    )
                 )
+
+                msg_col += styles["END"]
 
                 stream.write(msg_col)
                 stream.write(self.terminator)