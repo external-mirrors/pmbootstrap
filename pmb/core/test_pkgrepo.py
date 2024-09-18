fda06e7fd8e2395239c810854cdea9a5e273e898
Caleb Connolly <caleb@postmarketos.org>
2024-09-18 13:35:57 +0200

    test: disable some caches
    some of these break tests, disable pkgrepo_paths() cache globally and
    fix the systemd_selected() test.
    
    Signed-off-by: Caleb Connolly <caleb@postmarketos.org>

--- pmb/core/test_pkgrepo.py
+++ pmb/core/test_pkgrepo.py
@@ -3,6 +3,9 @@ import pytest
 import pmb.helpers.git
 import pmb.helpers.run
 from pmb.core.pkgrepo import pkgrepo_paths, pkgrepo_default_path
+from pmb.core.context import get_context
+import pmb.config.other
+import pmb.config.pmaports
 
 
 @pytest.mark.parametrize("config_file", ["no-repos"], indirect=True)
@@ -18,7 +21,8 @@ def test_pkgrepo_pmaports(pmaports, monkeypatch):
     """Test pkgrepo_paths() with pmaports repository and systemd extra repo"""
 
     # Disable results caching
-    pkgrepo_paths.cache_disable()
+    pmb.config.pmaports.read_config_repos.cache_disable()
+    pmb.config.is_systemd_selected.cache_disable()
 
     paths = pkgrepo_paths()
     print(f"[master] pkgrepo_paths: {paths}")
@@ -38,6 +42,9 @@ def test_pkgrepo_pmaports(pmaports, monkeypatch):
     )
 
     paths = pkgrepo_paths()
+    print(f"[master_staging_systemd] pkgrepo_paths: {paths}")
+    is_systemd, reason = pmb.config.other.systemd_selected_str(get_context().config)
+    print(f"config.systemd: {is_systemd}, {reason}")
     assert len(paths) == 2
 
     # systemd is the first path, since we want packages there to take priority
