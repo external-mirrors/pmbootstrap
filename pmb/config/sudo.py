905836e9ad3de404b909d29b7f5dc15f0cef10c0
Anjandev Momi <anjan@momi.ca>
2024-11-11 15:37:54 -0800

    test: add which_sudo unit tests

--- pmb/config/sudo.py
+++ pmb/config/sudo.py
@@ -2,10 +2,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import os
 import shutil
-from functools import lru_cache
+from pmb.meta import Cache
+from typing import Final
+
+PMB_SUDO_ENV_KEY: Final[str] = "PMB_SUDO"
 
 
-@lru_cache
+@Cache()
 def which_sudo() -> str | None:
     """Return a command required to run commands as root, if any.
 
@@ -17,11 +20,11 @@ def which_sudo() -> str | None:
 
     supported_sudos = ("doas", "sudo")
 
-    user_set_sudo = os.getenv("PMB_SUDO")
+    user_set_sudo = os.getenv(PMB_SUDO_ENV_KEY)
     if user_set_sudo is not None:
         if shutil.which(user_set_sudo) is None:
             raise RuntimeError(
-                "PMB_SUDO environmental variable is set to"
+                f"{PMB_SUDO_ENV_KEY} environmental variable is set to"
                 f" {user_set_sudo} but pmbootstrap cannot find"
                 " this command on your system."
             )
@@ -34,5 +37,5 @@ def which_sudo() -> str | None:
     raise RuntimeError(
         "Can't find sudo or doas required to run pmbootstrap."
         " Please install sudo, doas, or specify your own sudo"
-        " with the PMB_SUDO environmental variable."
+        f" with the {PMB_SUDO_ENV_KEY} environmental variable."
     )
---
+++ pmb/config/test_sudo.py
@@ -0,0 +1,41 @@
+import os
+import pytest
+from pmb.config.sudo import which_sudo, PMB_SUDO_ENV_KEY
+
+
+def test_sudo_override(monkeypatch):
+    """check sudo is used when PMB_SUDO_ENV_KEY is set to sudo"""
+    monkeypatch.setenv(PMB_SUDO_ENV_KEY, "sudo")
+    which_sudo.cache_disable()
+    assert which_sudo() == "sudo"
+
+
+def test_using_doas_default(monkeypatch):
+    """check doas is used when PMB_SUDO_ENV_KEY not defined"""
+
+    def doas_path():
+        return "/usr/bin/doas"
+
+    monkeypatch.delenv(PMB_SUDO_ENV_KEY, raising=False)
+    monkeypatch.setattr(shutil, "which", doas_path)
+    which_sudo.cache_disable()
+    assert which_sudo() == "doas"
+
+
+def test_bad_env(monkeypatch):
+    """check error is raised when PMB_SUDO_ENV_KEY is misspelled"""
+    monkeypatch.setenv(PMB_SUDO_ENV_KEY, "doass")
+    which_sudo.cache_disable()
+    with pytest.raises(RuntimeError):
+        which_sudo()
+
+
+def test_already_root(monkeypatch):
+    """which_sudo should be None if pmbootstrap ran as root"""
+
+    def root_getuid():
+        return 0
+
+    which_sudo.cache_disable()
+    monkeypatch.setattr(os, "getuid", root_getuid)
+    assert which_sudo() is None
