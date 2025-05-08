external-mirrors/pmbootstrap

commit d01f6b9079e7443de37e208b318a09eb4c278ce4

pmb.netboot: Only attempt to install nbd after rootfs existance check (MR 2540)
The install action will initialise the native chroot, so this way we
don't have to initialise the chroot in this code path. It also gives
users feedback quicker if they haven't already initialised the rootfs
since this way they don't have to wait around for nbd to be installed
before being told that they need to generate the rootfs first.

Closes https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/issues/2549

pmb/netboot/__init__.py
@@ -18,8 +18,6 @@ def start_nbd_server(device: str, replace: bool, ip: str = "172.16.42.2", port: 
     :param port: port of nbd server
     """
 
-    pmb.chroot.apk.install(["nbd"], Chroot.native())
-
     chroot = Chroot.native()
 
     rootfs_path = Path("/mnt/pmbootstrap/netboot") / f"{device}.img"
@@ -40,6 +38,8 @@ def start_nbd_server(device: str, replace: bool, ip: str = "172.16.42.2", port: 
             f'zap" for your convenience. Use "pmbootstrap netboot '
             f'serve --help" for more options.'
         )
+
+    pmb.chroot.apk.install(["nbd"], chroot)
 
     logging.info(f"Running nbd server for {device} on {ip} port {port}.")
 

commit 0bca7555d795d566ea4c0c9745bac235e360413c

pmb.netboot: Initialize chroot before checking path in chroot
Otherwise with zapped chroots, the check will fail and pmbootstrap won't
be able to locate the netboot image.

Fixes: d01f6b90 ("pmb.netboot: Only attempt to install nbd after rootfs existance check (MR 2540)")

Part-of: https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2583

pmb/netboot/__init__.py
@@ -20,6 +20,7 @@ def start_nbd_server(device: str, replace: bool, ip: str = "172.16.42.2", port: 
     """
 
     chroot = Chroot.native()
+    pmb.chroot.init(chroot)
 
     rootfs_path = Path("/mnt/pmbootstrap/netboot") / f"{device}.img"
     if not (chroot / rootfs_path).exists() or replace: