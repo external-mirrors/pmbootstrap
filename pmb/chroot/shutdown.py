2f8cbc390c15b3877e4e7e8551c5b79e743777df
Casey Connolly <kcxt@postmarketos.org>
2025-05-30 16:38:33 +0200

    chroot: shutdown: remove shutdown_cryptsetup_device()
    Cryptsetup will be handled by systemd-repart or a similar application
    in the future, and won't involve mounting/loop devices. So this code is
    not needed.
    
    Signed-off-by: Casey Connolly <kcxt@postmarketos.org>

--- pmb/chroot/shutdown.py
+++ pmb/chroot/shutdown.py
@@ -7,38 +7,7 @@ import pmb.helpers.mount
 from pmb.core import Chroot
 from pmb.core.context import get_context
 
-
-def shutdown_cryptsetup_device(name: str) -> None:
-    """
-    :param name: cryptsetup device name, usually "pm_crypt" in pmbootstrap
-    """
-    if not (Chroot.native() / "dev/mapper" / name).exists():
-        return
-    pmb.chroot.apk.install(["cryptsetup"], Chroot.native())
-    status = pmb.chroot.root(["cryptsetup", "status", name], output_return=True, check=False)
-    if not status:
-        logging.warning(
-            "WARNING: Failed to run cryptsetup to get the status"
-            " for " + name + ", assuming it is not mounted"
-            " (shutdown fails later if it is)!"
-        )
-        return
-
-    if status.startswith("/dev/mapper/" + name + " is active."):
-        pmb.chroot.root(["cryptsetup", "luksClose", name])
-    elif status.startswith("/dev/mapper/" + name + " is inactive."):
-        # When "cryptsetup status" fails, the device is not mounted and we
-        # have a left over file (#83)
-        pmb.chroot.root(["rm", "/dev/mapper/" + name])
-    else:
-        raise RuntimeError("Failed to parse 'cryptsetup status' output!")
-
-
 def shutdown(only_install_related: bool = False) -> None:
-    # Umount installation-related paths (order is important!)
-    # pmb.helpers.mount.umount_all(chroot / "mnt/install")
-    shutdown_cryptsetup_device("pm_crypt")
-
     # Remove "in-pmbootstrap" marker from all chroots. This marker indicates
     # that pmbootstrap has set up all mount points etc. to run programs inside
     # the chroots, but we want it gone afterwards (e.g. when the chroot
