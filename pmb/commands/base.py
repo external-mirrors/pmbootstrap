9f8c044c8f85d2cc2e106208fd0a0a6754766ca5
Stefan Hansson <newbyte@postmarketos.org>
2025-11-07 22:01:21 +0100

    pmb.commands.base: Add add_arguments() stub method
    To avoid having a massive unwieldy file for the argument parsing where
    command parser are detached from their implementations, let's allow
    command implementation classes to implement argument parsing by using
    this method.
    
    Originally suggested by Casey[1].
    
     [1]: https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2523#note_465484
    
    Signed-off-by: Stefan Hansson <newbyte@postmarketos.org>

--- pmb/commands/base.py
+++ pmb/commands/base.py
@@ -1,10 +1,17 @@
 # Copyright 2024 Caleb Connolly
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import argparse
+
 
 class Command:
     """Base class for pmbootstrap commands."""
 
     def run(self) -> None:
         """Run the command."""
+        raise NotImplementedError()
+
+    @staticmethod
+    def add_arguments(subparser: argparse._SubParsersAction) -> argparse.ArgumentParser:
+        """Set up the arguments for this command."""
         raise NotImplementedError()
