c46ebc3302837a3b7f4642de3f8b8db6ab1b8679
Stefan Hansson <newbyte@postmarketos.org>
2025-11-07 16:13:32 +0100

    pmb.parse._apkbuild: Remove attr-defined disable
    Type checking this file with mypy no longer fails with this enabled, so
    let's enable it.
    
    Signed-off-by: Stefan Hansson <newbyte@postmarketos.org>

--- pmb/parse/_apkbuild.py
+++ pmb/parse/_apkbuild.py
@@ -1,6 +1,5 @@
 # Copyright 2023 Oliver Smith
 # SPDX-License-Identifier: GPL-3.0-or-later
-# mypy: disable-error-code="attr-defined"
 from pmb.core.context import get_context
 from pmb.helpers import logging
 from pmb.types import Apkbuild
