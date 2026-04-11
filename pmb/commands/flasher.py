a38b7022e987272d49b96a05c9a6b494c8cc9c65
Stefan Hansson <newbyte@postmarketos.org>
2026-04-11 20:44:54 +0200

    pmb.commands.flasher: Use match statement
    Also update types to appease mypy.
    
    Signed-off-by: Stefan Hansson <newbyte@postmarketos.org>

--- pmb/commands/flasher.py
+++ pmb/commands/flasher.py
@@ -6,10 +6,11 @@ import pmb.parse.deviceinfo
 from pmb.core.context import get_context
 from pmb.flasher.frontend import flash_lk2nd, kernel, list_flavors, rootfs, sideload
 from pmb.helpers import logging
+from pmb.types import ActionFlasher
 
 
 def flasher(
-    action: str,
+    action: ActionFlasher,
     autoinstall: bool,
     cmdline: str | None,
     flash_method: str,
@@ -26,75 +27,76 @@ def flasher(
         logging.info("This device doesn't support any flash method.")
         return
 
-    if action in ["boot", "flash_kernel"]:
-        kernel(
-            deviceinfo,
-            method,
-            action == "boot",
-            autoinstall,
-            cmdline=cmdline,
-            no_reboot=no_reboot,
-            partition=partition,
-            resume=resume,
-        )
-    elif action == "flash_rootfs":
-        rootfs(
-            deviceinfo,
-            method,
-            cmdline=cmdline,
-            no_reboot=no_reboot,
-            partition=partition,
-            resume=resume,
-        )
-    elif action == "flash_vbmeta":
-        logging.info("(native) flash vbmeta.img with verity disabled flag")
-        pmb.flasher.run(
-            deviceinfo,
-            method,
-            "flash_vbmeta",
-            cmdline=cmdline,
-            no_reboot=no_reboot,
-            partition=partition,
-            resume=resume,
-        )
-    elif action == "flash_dtbo":
-        logging.info("(native) flash dtbo image")
-        pmb.flasher.run(
-            deviceinfo,
-            method,
-            "flash_dtbo",
-            cmdline=cmdline,
-            no_reboot=no_reboot,
-            partition=partition,
-            resume=resume,
-        )
-    elif action == "flash_lk2nd":
-        flash_lk2nd(
-            deviceinfo,
-            method,
-            cmdline=cmdline,
-            no_reboot=no_reboot,
-            partition=partition,
-            resume=resume,
-        )
-    elif action == "list_flavors":
-        list_flavors(device)
-    elif action == "list_devices":
-        pmb.flasher.run(
-            deviceinfo,
-            method,
-            "list_devices",
-            cmdline=cmdline,
-            no_reboot=no_reboot,
-            partition=partition,
-            resume=resume,
-        )
-    elif action == "sideload":
-        sideload(
-            deviceinfo,
-            method,
-            cmdline=cmdline,
-            no_reboot=no_reboot,
-            partition=partition,
-            resume=resume,
-        )
+    match action:
+        case "boot" | "flash_kernel":
+            kernel(
+                deviceinfo,
+                method,
+                action == "boot",
+                autoinstall,
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
+        case "flash_rootfs":
+            rootfs(
+                deviceinfo,
+                method,
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
+        case "flash_vbmeta":
+            logging.info("(native) flash vbmeta.img with verity disabled flag")
+            pmb.flasher.run(
+                deviceinfo,
+                method,
+                "flash_vbmeta",
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
+        case "flash_dtbo":
+            logging.info("(native) flash dtbo image")
+            pmb.flasher.run(
+                deviceinfo,
+                method,
+                "flash_dtbo",
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
+        case "flash_lk2nd":
+            flash_lk2nd(
+                deviceinfo,
+                method,
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
+        case "list_flavors":
+            list_flavors(device)
+        case "list_devices":
+            pmb.flasher.run(
+                deviceinfo,
+                method,
+                "list_devices",
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
+        case "sideload":
+            sideload(
+                deviceinfo,
+                method,
+                cmdline=cmdline,
+                no_reboot=no_reboot,
+                partition=partition,
+                resume=resume,
+            )
