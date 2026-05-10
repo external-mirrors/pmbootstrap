7e686f5998c6790e78c57417c341f3643b773dfc
Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
2026-05-10 13:00:02 +0200

    pmb.parse.apkindex: rename some functions and mark private
    Signed-off-by: Pablo Correa Gómez <pabloyoyoista@postmarketos.org>

--- pmb/parse/apkindex.py
+++ pmb/parse/apkindex.py
@@ -25,7 +25,7 @@ def _read_apkindex(path: Path) -> list[str]:
             return handle.read().split("\n\n")
 
 
-def parse_add_block(
+def _add_block_from_provides(
     ret: dict[str, dict[str, ApkPackage]],
     block: ApkPackage,
     provide: str | None = None,
@@ -57,7 +57,7 @@ def parse_add_block(
     ret[provide][pkgname] = block
 
 
-def parse_block_add_pkgname(
+def _add_block_from_pkgname(
     ret: dict[str, ApkPackage],
     block: ApkPackage,
 ) -> None:
@@ -166,11 +166,11 @@ def parse(
             continue
 
         if multiple_providers:
-            parse_add_block(ret, block, None)
+            _add_block_from_provides(ret, block, None)
             for provide in block.provides:
-                parse_add_block(ret, block, provide)
+                _add_block_from_provides(ret, block, provide)
         else:
-            parse_block_add_pkgname(ret, block)
+            _add_block_from_pkgname(ret, block)
 
     # Update the cache
     key = cache_key(path)
