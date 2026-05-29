7cfb332b9fbf8c2252b288c91d1387e08a82f6d1
Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
2026-05-29 18:20:37 +0200

    pmb.core.apkindex_block: make parsing of the block part of the class
    Removing it from pmb.parse.apkindex. This is another step into making
    pmbootstrap more object-oriented instead or being a set of scripts. It
    also allows to write more granular tests, as we can test the parsing
    of the block independently to the parsing of the APKINDEX.
    
    In the process of making the ApkindexBlock a more complete class, the
    __dict__ method that was only used serialize the class as a dictionary
    for the "apkindex_parse" command is removed, and replaced with an
    actual implementation of a JSONEncoder that can serialize the
    ApkindexBlock class.
    
    Signed-off-by: Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
    Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/2959>

--- pmb/commands/apkindex_parse.py
+++ pmb/commands/apkindex_parse.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 
 import pmb.parse.apkindex
+from pmb.core.apkindex_block import ApkindexBlockEncoder
 
 
 def apkindex_parse(apkindex_path: Path, package: str | list[str]) -> None:
@@ -11,6 +12,6 @@ def apkindex_parse(apkindex_path: Path, package: str | list[str]) -> None:
     if package:
         if package not in result:
             raise RuntimeError(f"Package not found in the APKINDEX: {package}")
-        print(json.dumps(result[package], indent=4, default=vars))
+        print(json.dumps(result[package], indent=4, cls=ApkindexBlockEncoder))
     else:
-        print(json.dumps(result, indent=4, default=vars))
+        print(json.dumps(result, indent=4, cls=ApkindexBlockEncoder))
--- pmb/core/apkindex_block.py
+++ pmb/core/apkindex_block.py
@@ -1,43 +1,163 @@
 # Copyright 2024 Stefan Hansson
 # SPDX-License-Identifier: GPL-3.0-or-later
-from dataclasses import asdict, dataclass
+from __future__ import annotations
+
+from json import JSONEncoder
+from typing import Any
 
 from pmb.core.arch import Arch
 
+apkindex_map = {
+    "A": "arch",
+    "D": "depends",
+    "o": "origin",
+    "P": "pkgname",
+    "p": "provides",
+    "k": "provider_priority",
+    "t": "timestamp",
+    "V": "version",
+}
 
-@dataclass
+required_apkindex_keys = ["arch", "pkgname", "version"]
+
+
 class ApkindexBlock:
-    """
-    "timestamp" and "origin" are not set for virtual packages (#1273).
-    We use that information to skip these virtual packages in parse().
-    """
+    """A representation of a package block as parsed from APKINDEX file."""
 
-    #: the architecture of the package
-    arch: Arch
-    #: dependencies for the package
-    depends: list[str]
-    #: the origin name of the package
-    origin: str | None
-    #: package name
-    pkgname: str
-    #: what this package provides
-    provides: list[str]
-    #: provider priority for the package
-    provider_priority: int | None
-    #: unix timestamp of the package build date/time
-    timestamp: str | None
-    #: package version
-    version: str
+    def __init__(
+        self,
+        arch: Arch,
+        depends: list[str],
+        origin: str | None,
+        pkgname: str,
+        provides: list[str],
+        provider_priority: int | None,
+        timestamp: str | None,
+        version: str,
+    ):
+        self._arch = arch
+        self._depends = depends
+        self._origin = origin
+        self._pkgname = pkgname
+        self._provides = provides
+        self._provider_priority = provider_priority
+        self._timestamp = timestamp
+        self._version = version
+
+    @classmethod
+    def from_block(cls, block_lines: list[str]) -> ApkindexBlock:
+        ret: dict[str, Any] = {}
+        required_found = 0  # Count the required keys we found
+        for line in block_lines:
+            # Parse keys from the mapping
+            key = apkindex_map.get(line[0])
+            if not key:
+                continue
+            if key in ret:
+                raise RuntimeError(f"Key {key} specified twice in block: {ret}")
+            if key in required_apkindex_keys:
+                required_found += 1
+            ret[key] = line[2:]
+        # Check for required keys
+        if required_found != len(required_apkindex_keys):
+            for key in required_apkindex_keys:
+                if key not in ret:
+                    raise RuntimeError(f"Missing required key '{key}' in block {ret}")
+            raise RuntimeError(
+                f"Expected {len(required_apkindex_keys)} required keys,"
+                f" but found {required_found} in block: {ret}"
+            )
+
+        # Format optional lists
+        for key in ["provides", "depends"]:
+            if key in ret and ret[key] != "":
+                # Ignore all operators for now
+                values = ret[key].split(" ")
+                ret[key] = []
+                for value in values:
+                    for operator in [">", "=", "<", "~"]:
+                        if operator in value:
+                            value = value.split(operator)[0]
+                            break
+                    ret[key].append(value)
+            else:
+                ret[key] = []
+        provider_priority = ret.get("provider_priority")
+        if provider_priority:
+            if not provider_priority.isdigit():
+                raise RuntimeError(
+                    f"Invalid provider_priority: '{provider_priority}' parsing block {ret}"
+                )
+            provider_priority = int(provider_priority)
+        else:
+            provider_priority = None
+
+        return cls(
+            arch=Arch.from_str(ret["arch"]),
+            depends=ret["depends"],
+            origin=ret.get("origin"),
+            pkgname=ret["pkgname"],
+            provides=ret["provides"],
+            provider_priority=provider_priority,
+            timestamp=ret.get("timestamp"),
+            version=ret["version"],
+        )
 
     @property
-    def __dict__(self) -> dict:
-        """This needs a manual implementation as the dataclass contains an enum."""
-        block_dict = asdict(self)
+    def arch(self) -> Arch:
+        """The architecture of the package."""
+        return self._arch
 
-        block_dict["arch"] = str(block_dict["arch"])
+    @property
+    def depends(self) -> list[str]:
+        """Dependencies for the package."""
+        return self._depends
 
-        return block_dict
+    @property
+    def origin(self) -> str | None:
+        """
+        The origin name of the package.
 
-    @__dict__.setter
-    def __dict__(self, new_value: dict) -> None:
-        raise AssertionError("Use dot operator access for ApkindexBlock")
+        This is unset in virtual packages.
+        """
+        return self._origin
+
+    @property
+    def pkgname(self) -> str:
+        """The package name."""
+        return self._pkgname
+
+    @property
+    def provides(self) -> list[str]:
+        """The package providers."""
+        return self._provides
+
+    @property
+    def provider_priority(self) -> int | None:
+        """The provider priority for the package."""
+        return self._provider_priority
+
+    @property
+    def timestamp(self) -> str | None:
+        """
+        The unix timestamp of the package build date/time.
+
+        This is unset in virtual packages.
+        """
+        return self._timestamp
+
+    @property
+    def version(self) -> str:
+        """The package version."""
+        return self._version
+
+
+# This is needed since "apkindex_parse" command requires ApkindexBlock to
+# be json-serializable
+class ApkindexBlockEncoder(JSONEncoder):
+    def default(self, o: Any) -> dict:
+        if isinstance(o, ApkindexBlock):
+            ret = {k[1:]: v for k, v in vars(o).items()}
+            ret["arch"] = str(ret["arch"])
+            return ret
+        return super().default(o)
