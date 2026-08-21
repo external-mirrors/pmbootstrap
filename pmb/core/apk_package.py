8e9501d986a6707006f8ca7f41aba9e97a296987
Stefan Hansson <newbyte@postmarketos.org>
2026-08-21 11:25:40 +0200

    pmb.core.apk_package: Update class docstring
    This is more apt given that this class no longer is specific to the
    APKINDEX parsing code.
    
    Fixes a171c9564ec8143572937251cf0d19a508a31c99
    
    Signed-off-by: Stefan Hansson <newbyte@postmarketos.org>
    Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/3032>

--- pmb/core/apk_package.py
+++ pmb/core/apk_package.py
@@ -25,7 +25,7 @@ Apkbuild = dict[str, Any]
 
 
 class ApkPackage:
-    """A representation of a package block as parsed from APKINDEX file."""
+    """Representation of a package as parsed from e.g. an APKINDEX file or an APKBUILD."""
 
     def __init__(
         self,
