external-mirrors/pmbootstrap

commit 11b2c4f4c6706552065ab281b287b87170135fca

pmb.core.config: Make "aports" variable a Path rather than a list[Path]
The original intention here was to support multiple repositories.
However, this was instead implemented through the "extra repos"
mechanism and much of pmbootstrap's code only grabs the last element
from the aports list anyway. Let's get rid of this mostly if not
entirely unused feature to allow simplifying pmbootstrap's code and ease
future development.

Signed-off-by: Stefan Hansson <newbyte@postmarketos.org>
Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/3034>

pmb/helpers/repo_missing.py
@@ -9,7 +9,7 @@ import pmb.build
 import pmb.helpers.package
 import pmb.helpers.pmaports
 from pmb.core.arch import Arch
-from pmb.core.context import get_context
+from pmb.core.pkgrepo import pkgrepo_paths
 from pmb.helpers.devices import DeviceCategory, get_device_category_by_apkbuild_path
 from pmb.meta import Cache
 from pmb.types import WithExtraRepos
@@ -43,7 +43,7 @@ def generate(arch: Arch) -> list[dict[str, list[str] | str | None]]:
     """
     ret = []
 
-    for pmaports_dir in get_context().config.aports:
+    for pmaports_dir in pkgrepo_paths(WithExtraRepos.ENABLED):
         pattern = os.path.join(pmaports_dir, "**/*/APKBUILD")
 
         for apkbuild_path_str in glob.glob(pattern, recursive=True):

pmb/config/init.py
@@ -939,16 +939,16 @@ def frontend(args: PmbArgs) -> None:
     # Work folder (needs to be first, so we can create chroots early)
     config = get_context().config
 
-    using_default_pmaports = config.aports[-1].is_relative_to(config.work)
+    using_default_pmaports = config.aports.is_relative_to(config.work)
 
     config.work, work_exists = ask_for_work_path(config.work)
 
     # If the work dir changed then we need to update the pmaports path
     # to be relative to the new workdir
     if using_default_pmaports:
-        config.aports = [config.work / "cache_git/pmaports"]
+        config.aports = config.work / "cache_git/pmaports"
 
-    config.aports[-1] = ask_for_pmaports_path(config.aports[-1])
+    config.aports = ask_for_pmaports_path(config.aports)
 
     # Update args and save config (so chroots and 'pmbootstrap log' work)
     # pmb.helpers.args.update_work(args, config.work)

pmb/core/config.py
@@ -48,11 +48,7 @@ class AutoZapConfig(enum.Enum):
 
 
 class Config:
-    # This is a class variable that gets treated as an instance variable. It's wrong, but since we
-    # only ever have one config (for now?) it doesn't cause any issues. Would be good to fix though.
-    aports: list[Path] = [  # ruff:ignore[mutable-class-default]
-        Path(os.path.expanduser("~") + "/.local/var/pmbootstrap/cache_git/pmaports")
-    ]
+    aports: Path = Path(os.path.expanduser("~") + "/.local/var/pmbootstrap/cache_git/pmaports")
     boot_size: int = 512
     build_default_device_arch: bool = False
     build_pkgs_on_install: bool = True
@@ -123,6 +119,14 @@ class Config:
         keys = key.split(".")
         if len(keys) == 1:
             type_ = type(getattr(Config, key))
+            try:
+                value = type_(value)
+            except TypeError:
+                # Handle transition from aports as list[Path] to Path.
+                if key == "aports" and isinstance(value, list):
+                    value = value[-1]
+                else:
+                    raise
             try:
                 if type_ is bool and isinstance(value, str):
                     if value.lower() in ["true", "false"]:

pmb/core/pkgrepo.py
@@ -15,11 +15,11 @@ from pmb.types import WithExtraRepos
 @Cache("with_extra_repos")
 def pkgrepo_paths(with_extra_repos: WithExtraRepos = WithExtraRepos.DEFAULT) -> list[Path]:
     config = get_context().config
-    paths = [Path(x) for x in config.aports]
-    if not paths:
-        raise RuntimeError("No package repositories specified?")
-
+    paths = [config.aports]
     with_systemd = False
+
+    if paths[0] == Path("."):
+        raise RuntimeError("No package repositories specified?")
 
     match with_extra_repos:
         case WithExtraRepos.DISABLED:
@@ -61,7 +61,7 @@ def pkgrepo_name(path: Path) -> str:
     MUST be used instead of "path.name" as we need special handling
     for the pmaports repository.
     """
-    if path == get_context().config.aports[-1]:
+    if path == get_context().config.aports:
         return "pmaports"
 
     return path.name
@@ -71,7 +71,7 @@ def pkgrepo_path(name: str) -> Path:
     """Return the absolute path to the package repository with the given name."""
     # The pmaports repo is always last, and we hardcode the name.
     if name == "pmaports":
-        return get_context().config.aports[-1]
+        return get_context().config.aports
 
     for aports in pkgrepo_paths():
         if aports.name == name:

test/conftest.py
@@ -178,15 +178,15 @@ def pmaports(pmb_args: None, monkeypatch: MonkeyPatch) -> None:
     # As an optimisation, we check the default workdir for pmaports
     # and clone it from there if it exists. This saves a bunch of bandwidth
     # and time.
-    if Config.aports[-1].exists():
+    if Config.aports.exists():
         # Override the URL to the local path so we can later look up the
         # remote by URL
-        pmb.config.git_repos["pmaports"] = [str(Config.aports[-1])]
+        pmb.config.git_repos["pmaports"] = [str(Config.aports)]
 
-    if not cfg.aports[-1].exists():
+    if not cfg.aports.exists():
         pmb.helpers.git.clone("pmaports")
         # Now operate on the cloned repo
-        assert pmb.helpers.run.user(["git", "switch", "main"], working_dir=cfg.aports[-1]) == 0
+        assert pmb.helpers.run.user(["git", "switch", "main"], working_dir=cfg.aports) == 0
 
 
 @pytest.fixture

commit 0b71fef901c723535b178412508b200db5cba3d9

repo_missing: fix packages appearing twice
Fix a regression from 11b2c4f4 ("pmb.core.config: Make "aports" variable
a Path rather than a list[Path]") that resulted in repo_missing
returning packages from the systemd repository twice, once as part of
the systemd repository (as it is supposed to be) and once as part of the
regular repository (wrong!).

I have noticed this because bpo refused the generated package lists
with:
  RuntimeError: pkgname found twice in payload with repo=None: gnome-settings-daemon-mobile

The gnome-settings-daemon-mobile package is really in both repositories:

  ./temp/gnome-settings-daemon-mobile
  ./extra-repos/systemd/gnome-settings-daemon-mobile

But due to the bug it appeared to be twice in the regular repository.
Adjust the code to fix this.

Signed-off-by: Oliver Smith <ollieparanoid@postmarketos.org>
Part-of: <https://gitlab.postmarketos.org/postmarketOS/pmbootstrap/-/merge_requests/3041>

pmb/helpers/repo_missing.py
@@ -43,7 +43,7 @@ def generate(arch: Arch) -> list[dict[str, list[str] | str | None]]:
     """
     ret = []
 
-    for pmaports_dir in pkgrepo_paths(WithExtraRepos.ENABLED):
+    for pmaports_dir in pkgrepo_paths(WithExtraRepos.DISABLED):
         pattern = os.path.join(pmaports_dir, "**/*/APKBUILD")
 
         for apkbuild_path_str in glob.glob(pattern, recursive=True):